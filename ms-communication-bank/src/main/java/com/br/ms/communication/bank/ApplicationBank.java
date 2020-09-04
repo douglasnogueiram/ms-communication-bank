f3080717f86c07e363e5fa01b55c7b28f2a52c4c
douglasnogueirademelo <douglasnogueirademelo@air-de-douglas>
2020-09-04 13:56:18 -0300

    Integração via Apache Kafka Inclusão de monitoração via Elastic APM.

--- ms-communication-bank/src/main/java/com/br/ms/communication/bank/ApplicationBank.java
+++ ms-communication-bank/src/main/java/com/br/ms/communication/bank/ApplicationBank.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
+
 @SpringBootApplication
 @EntityScan
 public class ApplicationBank {
 
 	public static void main(String[] args) {
+		ElasticApmAttacher.attach();
 		SpringApplication.run(ApplicationBank.class, args);
 	}	
 	
